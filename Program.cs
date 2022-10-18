b66e628481df14005cb0f6700447d5d8ae9d3562
Kenzodiac <kennethfujimura@gmail.com>
2022-10-18 15:26:37 -0700

    Ints to decimals and method insertion

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 //10-18-2022
 //GitHub Challenge # 2 - Add 2 Numbers
 //Console program greets the user, takes in two numbers as inputs, converts those numbers to integers, adds the numbers, and returns the sum value to the user.
-//Peer Review by: 
+//Peer Review by: Aman 
 
 
 Console.Clear();
@@ -12,37 +12,30 @@ Console.WriteLine("Let's add two numbers!");
 bool playAgain = true;
 while (playAgain == true) {
     //variable declaration
-    bool isConverted1 = false;
     decimal num1 = 0;
-
-    bool isConverted2 = false;
     decimal num2 = 0;
 
+    decimal InputAndValidation(decimal num){
+        bool isConverted = false;
+        while (!isConverted) {
+            string stringInput = Console.ReadLine();
+            bool isNumber = Decimal.TryParse(stringInput, out num);
+
+            if (isNumber == true){
+                isConverted = true;
+            } else {
+                Console.WriteLine("Invalid entry. Please enter a whole number or 'Integer'.");
+            }
+        }
+        return num;
+    }
     //first number input prompt with data verification
     Console.WriteLine("Enter the first number you wish to add: ");
-    while (!isConverted1) {
-        string stringInput = Console.ReadLine();
-        bool isNumber = Decimal.TryParse(stringInput, out num1);
-
-        if (isNumber == true){
-            isConverted1 = true;
-        } else {
-            Console.WriteLine("Invalid entry. Please enter a whole number or 'Integer'.");
-        }
-    }
+    num1 = InputAndValidation(num1);
     
     //second number input prompt with data verification
     Console.WriteLine("Enter the second number you wish to add: ");
-    while (!isConverted2) {
-        string stringInput = Console.ReadLine();
-        bool isNumber = Decimal.TryParse(stringInput, out num2);
-
-        if (isNumber == true){
-            isConverted2 = true;
-        } else {
-            Console.WriteLine("Invalid entry. Please enter a whole number or 'Integer'.");
-        }
-    }
+    num2 = InputAndValidation(num2);
 
     //sum calculation
     decimal sum = num1 + num2;
